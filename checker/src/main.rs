f6c725f4308aecc3b7a0596428ae58eb36d91610
Gaku10 <gaku.pr0g@gmail.com>
2021-02-01 22:37:14 +0100

    input.............

--- checker/src/main.rs
+++ checker/src/main.rs
@@ -1,27 +1,35 @@
+#![allow(unused)]
 extern crate rand;
 
-use std::io;
-use std::Rng;
-
-
+    use std::io;
+    use rand::prelude::*;
+    use rand::Rng;
 fn main() {
-    let mut chekers = rand::thread_rng(30,50);
+    let mut rng = rand::thread_rng();
+    let mut chekers: u16 = rng.gen_range(20..51);
     if chekers%4==1{
         chekers += 1;
     }
     loop{
         println!("There are {} chekers", chekers);
+        
         //player's number = input()
-        //chekers -= number;
+        let reader = io::stdin();
+        let mut input_text = String::new();
+        reader.read_line(&mut input_text).expect("failed to read line");
+        let number = input_text.trim().parse::<u16>();
+
+
+        chekers -= number;
         if chekers==0{
             println!("You lose");
             break;
         }
         
         if chekers%4==1{
-            //chekers -= 4-number;
+            chekers -= 4-number;
         }else{
-        chekers -= rand::thread_rng(1,3);
+        chekers -= rng.gen_range(20..51);
     }
         if chekers==0{
             println!("You win");
